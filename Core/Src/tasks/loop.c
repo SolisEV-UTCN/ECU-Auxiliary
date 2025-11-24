fedaef49c63af4ec9b818ccc0bc469cd0a52870f
XBoom31 <relubadea333@gmail.com>
2025-11-24 18:37:13 +0200

    code comments

--- Core/Src/tasks/loop.c
+++ Core/Src/tasks/loop.c
@@ -1,10 +1,10 @@
 #include"main.h"
 
-void loop()
-{
-	 static uint8_t status = 0;
-
-	 HAL_GPIO_WritePin(GPIOA, HEARTH_BEAT_Pin, status);
-	 status = !status;
-	 HAL_Delay(500);
+//this is the main loop
+void loop() {
+	// we are flipping the hearthbeat led every 500ms
+	static uint8_t status = 0;
+	HAL_GPIO_WritePin(GPIOA, HEARTH_BEAT_Pin, status);
+	status = !status;
+	HAL_Delay(500);
 }
